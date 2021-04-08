73985882edf9c169b45c4e0c0dc837158c9a57df
user <you@example.com>
2021-04-08 12:55:23 +0500

    lab3

--- lab3.1.c
+++ lab3.1.c
@@ -2,22 +2,23 @@
 #include <math.h>
 int main()
 {
-    double x0, x, h, f, i, n;
+    double x0, x, h, f;
+    int i, n;
     printf("Vvedite shag h: ");
     scanf("%lf", &h);
     printf("x\t\t f(x)\n");
     printf("-----------------------\n");
-    x0 = -1;
+    x0 = -1.0;
     n = (2 - (-1)) / h;
     i = 0;
     while (i <= n)
     {
         x = x0 + i * h;
-        if (x <= 1)
-            f = exp(-2 * sin(x));
+        if (x <= 1.0)
+            f = exp(-2.0 * sin(x));
         else
-            f = x * x - (1 / tan(x));
-        printf("%5.3lf\t\t %6.4lf\n", x, f);
+            f = x * x - (1.0 / tan(x));
+        printf("%lf\t\t %6.4lf\n", x, f);
         i += 1;
     }
     return 0;
--- lab3.2.c
+++ lab3.2.c
@@ -2,21 +2,22 @@
 #include <math.h>
 int main()
 {
-    double x0, x, h, f, i, n;
+    double x0, x, h, f;
+    int i, n;
     printf("Vvedite shag h: ");
     scanf("%lf", &h);
     printf("x\t\t f(x)\n");
     printf("-----------------------\n");
-    x0 = -1;
+    x0 = -1.0;
     n = (2 - (-1)) / h;
     for (i = 0; i <= n; i++)
     {
         x = x0 + i * h;
-        if (x <= 1)
-            f = exp(-2 * sin(x));
+        if (x <= 1.0)
+            f = exp(-2.0 * sin(x));
         else
-            f = x * x - (1 / tan(x));
-        printf("%5.3lf\t\t %6.4lf\n", x, f);
+            f = x * x - (1.0 / tan(x));
+        printf("%lf\t\t %6.4lf\n", x, f);
     }
     return 0;
 }
