8a565ad093f5c3afb9906edec2328a85f7ff1e3a
user <user@ws1013402138.pm>
2021-04-16 18:35:32 +0500

    laba6

--- lab6.c
+++ lab6.c
@@ -7,6 +7,12 @@ void fill(int n, int a[])
     for (i = 0; i < n; i++)
         a[i] = rand() % 21 - 10;
 }
+int out(int n, int a[])
+{
+    for (int i = 0; i < n; i++)
+        printf("%d\t", a[i]);
+    return 0;
+}
 int el(int n, int m, int a[])
 {
     int i, index = 100;
@@ -20,49 +26,51 @@ int el(int n, int m, int a[])
     }
     return index;
 }
+int obmen1(int n, int index, int a[])
+{
+    for (int i = 0; i < index / 2; i++)
+    {
+        int k = a[i];
+        a[i] = a[index - i - 1];
+        a[index - i - 1] = k;
+    }
+    for (int i = index + 1; i < n - (n - index) / 2; i++)
+    {
+        int l = a[i];
+        a[i] = a[n - i + index];
+        a[n - i + index] = l;
+    }
+    return 0;
+}
+int obmen2(int n, int a[])
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        int j = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = j;
+    }
+    return 0;
+}
 int main()
 {
     srand(time(NULL));
-    int n, a[n], i, m, index, k, l, j;
+    int n, m, index;
     printf("Vvedite n: ");
     scanf("%d", &n);
+    int a[n];
     fill(n, a);
-    for (i = 0; i < n; i++)
-        printf("%d\t", a[i]);
+    out(n, a);
     printf("\n");
     printf("Vvedite element:");
     scanf("%d", &m);
     index = el(n, m, a);
-    printf("index = %d\n", index));
+    printf("index = %d\n", index);
     if (index != 100)
-    {
-        for (i = 0; i < index / 2; i++)
-        {
-            k = a[i];
-            a[i] = a[index - i - 1];
-            a[index - i - 1] = k;
-        }
-
-        for (i = index + 1; i < n - index; i++)
-        {
-            l = a[i];
-            a[i] = a[n - i + index];
-            a[n - i + index] = l;
-        }
-    }
+        obmen1(n, index, a);
     else
-    {
-        for (i = 0; i < n / 2; i++)
-        {
-            j = a[i];
-            a[i] = a[n - i - 1];
-            a[n - i - 1] = j;
-        }
-    }
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\t", a[i]);
-    }
+        obmen2(n, a);
+    out(n, a);
     printf("\n");
     return 0;
 }
