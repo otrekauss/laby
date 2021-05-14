99e4f6540adcea42587bd952b1b21bfd9b5dd44f
user <user@ws1013402131.pm>
2021-05-14 19:30:10 +0500

    lab10

--- lab10.c
+++ lab10.c
@@ -1,71 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int fill(int n, int m, int a[n][m])
+int fill(int n, int m, int *a)
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            a[i][j] = rand() % 31 - 15;
+            a[i * m + j] = rand() % 31 - 15;
     return 0;
 }
-int sum(int n, int m, int a[n][m])
+int sum(int n, int m, int *a)
 {
     int k;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
         {
             if ((i == j) || (i - j == 1) || (j - i == 1))
-                k += a[i][j];
+                k += a[i * m + j];
         }
     return k;
 }
-int transp(int n, int m, int a[n][m], int b[m][n])
+int transp(int n, int m, int *a, int *b)
 {
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
         {
-            b[i][j] = a[j][i];
+            b[i * m + j] = a[j * n + i];
+            // p = a[j * n + i];
+            // a[j * n + i] = a[i * m + j];
+            // a[i * m + j] = p;
         }
     return 0;
 }
-// int transp(int n, int m, int a[m][n])
-// {
-//     int p;
-//     for (int j = 0; j < m; j++)
-//         for (int i = 0; i < j; i++)
-//         {
-//             p = a[j][i];
-//             a[j][i] = a[i][j];
-//             a[i][j] = p;
-//         }
-//     return 0;
-//  }
-int nuli(int n, int m, int a[n][m])
+int nuli(int n, int m, int *a)
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
         {
             if ((i != j) && (i - j != 1) && (j - i != 1))
-                a[i][j] = 0;
+                a[i * m + j] = 0;
         }
     return 0;
 }
-int out1(int n, int m, int a[n][m])
+int out1(int n, int m, int *a)
 {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
-            printf("% 7d", a[i][j]);
+            printf("% 7d", a[i * m + j]);
         printf("\n");
     }
     return 0;
 }
-int out2(int n, int m, int a[m][n])
+int out2(int n, int m, int *a)
 {
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
-            printf("% 7d", a[i][j]);
+            printf("% 7d", a[i * m + j]);
         printf("\n");
     }
     return 0;
@@ -73,11 +64,21 @@ int out2(int n, int m, int a[m][n])
 int main()
 {
     srand(time(NULL));
-    int n, m, k, h;
+    int *a, *b, n, m, k, h;
     printf("Vvedite n, m: ");
     scanf("%d%d", &n, &m);
-    int a[n][m];
-    int b[m][n];
+    a = (int *)malloc(n * m * sizeof(int));
+    b = (int *)malloc(n * m * sizeof(int));
+    if (NULL == a)
+    {
+        printf("Memory allocation error!\n");
+        exit(EXIT_FAILURE);
+    }
+    if (NULL == b)
+    {
+        printf("Memory allocation error!\n");
+        exit(EXIT_FAILURE);
+    }
     fill(n, m, a);
     out1(n, m, a);
     k = sum(n, m, a);
@@ -92,5 +93,6 @@ int main()
         out1(n, m, a);
     else
         out2(n, m, b);
+    free(a);
     return 0;
 }
--- lab9.c
+++ lab9.c
@@ -1,10 +1,20 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-  int k;
-  char str[50] = "you4";
-  char delim[3]  ", ";
-  char a[*i] = strtok(str,delim);
-
+    int i, p, k = 0;
+    char str[256];
+    printf("stroka: ");
+    scanf("%255[^\n]", str);
+    p = strlen(str);
+    for (i = 0; i < p; i++)
+    {
+        if (isdigit(str[i]) && (isalpha(str[i + 1]) || str[i + 1] == ' ') && isdigit(str[i-1]) == 0)
+            k += (int)str[i] - 48;
+        if (isdigit(str[i]) && isdigit(str[i + 1]))
+            k += ((int)str[i] - 48) * 10 + ((int)str[i + 1] - 48);
+    }
+    printf("%d\n", k);
+    return 0;
 }
